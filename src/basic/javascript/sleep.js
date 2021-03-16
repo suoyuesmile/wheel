e88283c31ecacf9385dc60dcab89c15f49b72acb
shaosuo <suoyuesmile@163.com>
2021-03-16 18:57:05 +0800

    feat: add sleep

---
+++ src/basic/javascript/sleep.js
@@ -0,0 +1,10 @@
+function sleep(milliseconds) {
+    return new Promise((resolve => setTimeout(resolve, milliseconds)));
+}
+
+// test: only browser
+(async function test() {
+    console.log('start!');
+    await sleep(2000);
+    console.log('end!');
+})();
